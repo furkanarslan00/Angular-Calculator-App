93350dd3169b7008530994a69b2d269c4b65196c
Furkan Arslan <furkan0tr0arslan@gmail.com>
2024-07-05 10:43:21 +0300

    Angular-Calculator-App
    Calculator application prepared for Angular training in Enoca internship.

---
+++ README.md
@@ -0,0 +1,18 @@
+Calculator Application
+
+This project is a calculator application developed using Angular. The application is used to perform basic arithmetic operations and offers a modern user interface.
+
+Features:
+
+- Basic arithmetic operations (addition, subtraction, multiplication, division)
+- Dynamic display of results on the screen
+- Viewing and managing operation history
+- Dark mode support
+- Formatting of input and results
+
+Technologies and Techniques Used:
+
+- Angular: Used as the primary framework of the application.
+- TypeScript: Used throughout the codebase as a type-safe version of JavaScript.
+- HTML: Used to create the user interface.
+- CSS: Used for styling and layout of the application.
---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,100 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css'],
+})
+
+
+/*******************************************************************************************/
+export class CalculatorComponent {
+  title = 'Hesap Makinesi';
+  currentInput: string = '';
+  displayOperation: string = '';
+  result: number | null = null;
+  history: string[] = [];
+  showHistory: boolean = false;
+  isDarkMode: boolean = false;
+
+  get displayValue() {
+    if (this.result !== null) {
+      const resultString = String(this.result);
+      const maxSpaces = 28; 
+      const length = resultString.length;
+      
+      const spacesNeeded = Math.max(maxSpaces - 2 * (length - 1), 0);
+      
+      const spaces = ' '.repeat(spacesNeeded);
+      return `=${spaces}${this.result}`;
+    }
+    return '';
+  }
+  
+  appendNumber(num: number | string) {
+    this.currentInput += num.toString();
+    this.displayOperation += num.toString();
+  }
+
+  setOperator(op: string) {
+    let displayOp = op;
+    if (op === '/') {
+      displayOp = '÷';
+    } else if (op === '*') {
+      displayOp = '×';
+    }
+  
+    if (this.currentInput !== '') {
+      this.currentInput += ` ${op} `;
+      this.displayOperation += ` ${displayOp} `;
+    }
+  }
+
+  clear() {
+    this.currentInput = '';
+    this.displayOperation = '';
+    this.result = null;
+  }
+
+  toggleSign() {
+    if (this.currentInput !== '') {
+      let num = parseFloat(this.currentInput);
+      this.currentInput = String(-num);
+      this.displayOperation = this.currentInput;
+    }
+  }
+
+  percentage() {
+    if (this.currentInput !== '') {
+      let num = parseFloat(this.currentInput);
+      this.currentInput = String(num / 100);
+      this.displayOperation = this.currentInput;
+    }
+  }
+
+  calculate() {
+    if (this.currentInput !== '') {
+      try {
+        const result = eval(this.currentInput);
+        if (typeof result === 'number' && !isNaN(result)) {
+          this.result = result;
+          this.history.unshift(`${this.displayOperation} = ${this.result}`);
+          this.history = this.history.slice(0, 3);
+        } else {
+          console.error('Invalid expression');
+        }
+      } catch (error) {
+        console.error('Invalid expression', error);
+      }
+    }
+  }
+  
+
+  toggleHistoryPanel() {
+    this.showHistory = !this.showHistory;
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+  }
+}
